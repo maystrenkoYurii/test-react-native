fe5a11a1d9966fe0cf590c1dfc67533fb234a7ba
yurii_maistrenko <yura1994hd@gmail.com>
2021-02-28 17:58:54 +0200

    init root navigator

--- src/components/presentational/Screens/Main/index.tsx
+++ src/components/presentational/Screens/Main/index.tsx
@@ -1,9 +1,44 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import type { FunctionComponent } from 'react';
 
+import Users from '@presentational/Screens/Main/Users';
+import Followers from '@presentational/Screens/Main/Followers';
+
+import { getNavigatorOptions } from '@components/navigation';
+import { useCustomTheme } from '@hooks/theme';
+import { constants } from '@core/constants';
+
+import type { MainStackParams } from '@typings/navigation';
+
+const MainStack = createStackNavigator<MainStackParams>();
+
 const Main: FunctionComponent = () => {
-  return <Fragment />;
+  const theme = useCustomTheme();
+
+  const insets = useSafeAreaInsets();
+
+  return (
+    <MainStack.Navigator headerMode="screen" screenOptions={getNavigatorOptions(theme, insets)}>
+      <MainStack.Screen
+        component={Users}
+        name={constants.SCREEN_USERS}
+        options={{
+          headerTitle: 'Users',
+        }}
+      />
+
+      <MainStack.Screen
+        component={Followers}
+        name={constants.SCREEN_FOLLOWERS}
+        options={{
+          headerTitle: 'Followers',
+        }}
+      />
+    </MainStack.Navigator>
+  );
 };
 
 export default Main;
